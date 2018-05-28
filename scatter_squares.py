34083dda762fd113b3b27f9eec204f92ab88fc64
Jeter <Jeter@jeterMBP.local>
2018-05-28 09:39:29 +0800

    15.2.9

--- scatter_squares.py
+++ scatter_squares.py
@@ -1,8 +1,8 @@
 import matplotlib.pyplot as plt 
 
-x_value=[1,2,3,4,5]
-y_value=[1,4,9,16,25]
-plt.scatter(x_value,y_value,s=200)
+x_value=list(range(1,1001))
+y_value=[x**2 for x in x_value]
+plt.scatter(x_value,y_value,c=y_value,cmap=plt.cm.Blues,edgecolor='none',s=10)
 
 
 # 设置图表标题并给坐标轴加上标签 
@@ -13,4 +13,7 @@ plt.ylabel("Square of Value", fontsize=14)
 # 设置刻度标记的大小 
 plt.tick_params(axis='both', which='major', labelsize=14)
 
-plt.show()
+plt.axis([0,1100,0,1100000])
+
+# plt.show()
+plt.savefig('squares_plot.png',bbox_inches='tight')
