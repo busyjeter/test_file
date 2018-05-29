7cc0f154054edbedca07254bb593e1b5f032893a
Jeter <Jeter@jeterMBP.local>
2018-05-29 11:46:42 +0800

    17.2

--- python_repos.py
+++ python_repos.py
@@ -1,4 +1,7 @@
 import requests
+import pygal
+from pygal.style import LightColorizedStyle as LCS, LightenStyle as LS
+
 
 url = 'https://api.github.com/search/repositories?q=language:python&sort=stars'
 r=requests.get(url)
@@ -9,11 +12,20 @@ response_dict=r.json()
 print(response_dict.keys())
 print("total_count: ",response_dict['total_count'])
 
-repo_dicts=response_dict['items']
-print(len(repo_dicts))
 
-print("\nSelected information about each repository:")
-repo_dict=repo_dicts[0]
-print("keys:",len(repo_dict))
-for key in sorted(repo_dict.keys()):
-	print(key)
+repo_dicts=response_dict['items']
+# print(len(repo_dicts))
+names,stars=[],[]
+
+for repo_dict in repo_dicts:
+	names.append(repo_dict['name'])
+	stars.append(repo_dict['stargazers_count'])
+
+# 可视化
+my_style=LS('#333366',base_style=LCS)
+chart =pygal.Bar(style=my_style,x_label_rotation=45, show_legend=False)
+chart.title = 'Most-Starred Python Projects on GitHub'
+chart.x_labels=names
+
+chart.add('', stars) 
+chart.render_to_file('python_repos.svg')
