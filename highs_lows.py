1ad7edad941c67b916b928b654f75c81ea9931ab
Jeter <Jeter@jeterMBP.local>
2018-05-29 10:10:27 +0800

    16

--- highs_lows.py
+++ highs_lows.py
@@ -2,24 +2,30 @@ import csv
 from datetime import datetime
 from matplotlib import pyplot as plt
 
-filename='sitka_weather_07-2014.csv'
+filename='death_valley_2014.csv'
 with open(filename) as f:
 	reader=csv.reader(f)
 	header_row=next(reader)
 
-	dates,highs=[],[]
+	dates,highs,lows=[],[],[]
 	for row in reader:
-		current_date=datetime.strptime(row[0],"%Y-%m-%d")
-		dates.append(current_date)
-		high=int(row[1])
-		highs.append(high)
+		try:
+			current_date=datetime.strptime(row[0],"%Y-%m-%d")
+			high=int(row[1])
+			low=int(row[3])
+		except ValueError:
+			print(current_date,'missing data')
+		else:
+			dates.append(current_date)
+			highs.append(high)
+			lows.append(low)
 
-	print(highs)
 
 fig=plt.figure(figsize=(10,6))
-plt.plot(dates,highs,c='red')
-
-plt.title("Daily high temperatures, July 2014", fontsize=24)
+plt.plot(dates,highs,color='red')
+plt.plot(dates,lows,c='blue')
+plt.fill_between(dates,highs,lows,facecolor='blue',alpha=0.5)
+plt.title("Daily high temperatures,  2014", fontsize=24)
 plt.xlabel('', fontsize=16) 
 # fig.autofmt_xdate()
 plt.ylabel("Temperature (F)", fontsize=16) 
---
+++ world_population.py
@@ -0,0 +1,24 @@
+import json
+from country_codes import get_country_code
+import pygal.maps.world
+
+
+filename='population_data.json'
+with open(filename) as f:
+	pop_data=json.load(f)
+
+cc_populations={}
+for pop_dict in pop_data:
+	if pop_dict['Year']=='2010':
+		country_name=pop_dict['Country Name']
+		population=int(float(pop_dict['Value']))
+		code =get_country_code(country_name)
+		if code:
+			# print(code+' '+str(population))
+			cc_populations[code]=population
+		else:
+			print('Error'+country_name)
+wm = pygal.maps.world.World()
+wm.title = 'World Population in 2010, by Country'
+wm.add('2010', cc_populations)
+wm.render_to_file('world_population.svg')
